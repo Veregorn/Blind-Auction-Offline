ce9514dfffd7c001537bd7a7ef4049290fa020e8
Veregorn <rjbarco@gmail.com>
2023-09-12 12:52:53 +0200

    Main loop created to allow saving more than one bidder

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 import art
+import os
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 print(art.logo)
+
+auction_finished = False
+bids = {}
+user_response = ""
+
+while not auction_finished:
+
+    name = input("What is your name?: ")
+
+    bid = float(input("What is your bid?: $"))
+
+    bids[name] = bid
+
+    while (user_response != "yes" and user_response != "no"):
+
+        user_response = input("Are there more bidders? Write 'yes' or 'no': ")
+
+    if user_response == "no":
+        auction_finished = True
+    else:
+        clear_console()
+        user_response = ""
+
+def calculate_highest_bidder():
+    highest_bidder = bids[name]
